614f10d80b4cba82775bd9bda78d478193cdafc7
Nipuna Bhayani <nipunabhayani@yahoo.com>
2014-10-03 11:57:21 -0700

    Refactor code

--- src/main/java/com/vmware/Employee.java
+++ src/main/java/com/vmware/Employee.java
@@ -1,20 +1,23 @@
 package com.vmware;
 
-import java.util.regex.Matcher;
+
 import java.util.regex.Pattern;
-
-
 
 public class Employee {
 	private String name;
 	private String ssn;
 	private Pattern pattern;
 	
-	public Employee() {
-	  pattern = Pattern.compile("\\d{3}-\\d{2}-\\d{4}");
-	   
-		
+	public Employee(Pattern pattern) {
+		this.pattern = pattern;
 	}
+	
+	/*
+	public Employee() {
+		pattern = Pattern.compile("\\d{3}-\\d{2}-\\d{4}");
+
+	}
+*/
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -25,15 +28,15 @@ public class Employee {
 
 	public void setSocialSecurityNumber(String ssn) {
 		// TODO Auto-generated method stub
-		if(!(pattern.matcher(ssn).matches()))
-		 throw new IllegalArgumentException(ssn + " is not a valid SSN");
-		 this.ssn = ssn;
-		
+		if (!(pattern.matcher(ssn).matches()))
+			throw new IllegalArgumentException(ssn + " is not a valid SSN");
+		this.ssn = ssn;
+
 	}
 
 	public String getSocialSecurityNumber() {
 		// TODO Auto-generated method stub
-	
+
 		return ssn;
 	}
 }
--- src/test/java/com/vmware/EmployeeTest.java
+++ src/test/java/com/vmware/EmployeeTest.java
@@ -1,51 +1,53 @@
 package com.vmware;
 
-import static org.testng.Assert. *;
+import static org.testng.Assert.*;
+
+import java.util.regex.Pattern;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class EmployeeTest {
-	private	 Employee employee;
-	
+	private Employee employee;
+
 	@BeforeMethod
 	public void setUp() {
-		System.out.println("Construct a new Employee per test");
-		employee = new Employee();
-}
-	
+		employee = new Employee(Pattern.compile("\\d{3}-\\d{2}-\\d{4}"));
+		
+	}
+
 	@Test
 	public void createEmployeeAndGetName() {
 		String name = "Nipuna Bhayani";
 		employee.setName(name);
-		assertEquals(employee.getName(),name);
-			
-		
+		assertEquals(employee.getName(), name);
+
 	}
-	
+
 	@Test
 	public void addASocialSecurityNumber1() {
 		employee.setSocialSecurityNumber("123-45-6789");
-		assertEquals(employee.getSocialSecurityNumber(),"123-45-6789");
+		assertEquals(employee.getSocialSecurityNumber(), "123-45-6789");
 	}
+
 	@Test
 	public void addASocialSecurityNumber() {
 		employee.setSocialSecurityNumber("123-45-6710");
-		assertEquals(employee.getSocialSecurityNumber(),"123-45-6710");
+		assertEquals(employee.getSocialSecurityNumber(), "123-45-6710");
 	}
-	
+
 	@Test
 	public void fix0001_SocialSecurityMustBeACertainFormat() {
-		String badSSN ="Ramlikesxxxx";
-		
+		String badSSN = "Ramlikesxxxx";
+
 		try {
 			employee.setSocialSecurityNumber(badSSN);
 			fail("This line should not be reached");
 		} catch (IllegalArgumentException iae) {
 			assertEquals(iae.getMessage(), "Ramlikesxxxx is not a valid SSN");
 		}
-		
+
 	}
 
 }
